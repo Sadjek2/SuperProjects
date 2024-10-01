99f4480cb75778ca4f8869fc41e9518744ddb9b1
DanilSizz <theghost.teenager@gmail.com>
2024-10-01 12:53:44 +0300

    newwww ver of hangman

--- tortureGame/game_logic.py
+++ tortureGame/game_logic.py
@@ -1,44 +1,35 @@
 import random
 
-#выбор случайного слова из списка (list_of_words) и
-#возвращение выбранного слова
-def select_random_word(list_of_words: list):
-    return random.choice(list_of_words)
+#список слов на тему природа
+nature_words = ['лесопарк', 'горы', 'озеро', 'пещера', 'водопады', 'ландшафт', 'природный', 'речной', 'лесной', 'гористый', 'каменист', 'деревня', 'птичник', 'рыболов', 'экосистема']
+#случайно выбранное слово из списка nature_words
+selected_word = random.choice(nature_words)
+unique_letters = set(selected_word)
+#количество попыток (жизней) для отгадывания слова, исходя из кол-ва
+#уникальных букв в слове
+attemtps = len(unique_letters)
+guessed_letters = ["_"] * len(selected_word)
 
-#подсчёт уникальных символов в слове
-def count_unique_letters(word):
-    return len(set(word))
+#print(selected_word, attemtps, unique_letters)
 
-#пользовательский ввод - вводится строка и берётся первый символ,
-#если их больше
-def user_input():
-    return input("Введите букву")[:1]
-
-#подсчёт попыток 
-def count_of_attempts(attempts):
-    pass
-
-#нахождение букв (c) в слове (word) и
-#возвращение их индексов (indicies)
-def letter_indices(c: str, word: str):
-    #явная типизация данных (c: str, word: str)
+def find_unique_letter(c, word):
     indices = []
-    #индекс буквы в слове
-    #по длине слова
-    for i in range(len(word)):
-        if word[i] == c: 
-            indices.append(indices)
+    index = word.find(c)
+    while index != -1:
+        indices.append(index)
+        index = word.find(c, index + 1)
     return indices
-        
-def find_letter_in_set(c: str, unique_letters_set: set):
-    if c in unique_letters_set:
-        return True
-    else:
-        return False
+
+for i in range(attemtps):
+    letter = input()[:1]
     
-def replace_unguessed(indices: list, unguessed_list, c):
-    for i in zip(indices, unguessed_list):
-        unguessed_list[indices] = c
+    if letter not in unique_letters:
+        attemtps -= 1
+        print("Осталось попыток:", attemtps)
+        print(guessed_letters)
+    elif letter in unique_letters:
+        
+
         
         
         
